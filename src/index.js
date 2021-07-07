6d0d1eeb2507cb7e2afc664cc0c58bedcf9d8d12
“yfq <yfq000@126.com>
2021-07-07 08:35:20 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const navdata = [
+    {text: "水和水蒸气", child: [
+        {text: "物性查询", url: "/waterProperty"},
+        {text: "混合器", url: "/waterMixer"}
+    ]},
+    {text: "测试", child: [
+        {text: "测试", url: "/test"}
+    ]}
+];
+
+
+function initNav(data) {
+    var ul = document.createElement("ul");
+    
+    data.forEach(plist => {
+       
+        var ol = document.createElement("ol");
+        plist.child.forEach(clist => {
+            var a = document.createElement("a");
+            a.textContent = clist.text;
+            a.href = '#' + clist.url;
+            var li = document.createElement("li");
+            li.appendChild(a);
+            ol.appendChild(li);
+        });
+        
+        var a = document.createElement("a");
+        a.textContent = plist.text;
+        a.href = "javascript:;"
+        var li = document.createElement("li");
+        li.appendChild(a);
+        li.appendChild(ol);
+        ul.appendChild(li);
+    });
+    
+    return ul;
+}
+
+var home = function () {
+    document.getElementById("nav").innerHTML = '';
+    document.getElementById("nav").appendChild(initNav(navdata));
+}();
+
+window.Router.route("/", function () {
+    home();
+});
+
+window.Router.route("/test", function () {
+    alert("test");
+});
