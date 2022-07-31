885a5c4d12aeb1b89f0e9aca1ee32200305765c4
keith <yfq000@126.com>
2022-07-31 13:58:20 +0800

    m

--- 3/dist/page.js
+++ 3/dist/page.js
@@ -49,7 +49,7 @@ window.Router.route("page1", function () {
         row.do_ = new Number(do_) / 1000;
         row.thk = new Number(rowEle.getElementsByTagName("input")[1].value) / 1000;
         row.insulThk = new Number(rowEle.getElementsByTagName("input")[2].value) / 1000;
-        row.len = new Number(rowEle.getElementsByTagName("input")[3].value);
+        row.length = new Number(rowEle.getElementsByTagName("input")[3].value);
 
         let pipe = new Pipe();
         pipe.do_ = row.do_;
@@ -65,35 +65,33 @@ window.Router.route("page1", function () {
 
         rows.push(row);
     }
-    console.log(rows);
-    
     
     let table = document.createElement("table");
     table.innerHTML = "<tr><td>外径<br>mm</td><td>单重<br>kg/m</td><td>总重<br>kg</td><td>刷漆面积<br>m2</td><td>保温材料体积<br>m3</td><td>保护层面积<br>m3</td></tr>";
     let weightSum = 0, areaSum = 0, insulSum = 0, kapSum = 0;
-    len = rows.length;
-    for (let index = 0; index < len; index++) {
+
+    for (let index = 0; index < rows.length; index++) {
         const row = rows[index];
         let tr = document.createElement("tr");
         tr.innerHTML = 
-            "<td>" + (row.do_ * 1000).toString() + "</td>"
+            "  <td>" + (row.do_ * 1000).toString() + "</td>"
             + "<td>" + row.weight.toFixed(2) + "</td>"
-            + "<td>" + (row.weight * row.len).toFixed(2) + "</td>"
-            + "<td>" + (row.area * row.len).toFixed(2) + "</td>"
-            + "<td>" + (row.insulVolume * row.len).toFixed(2) + "</td>"
-            + "<td>" + (row.kapArea * row.len).toFixed(2) + "</td>";
+            + "<td>" + (row.weight * row.length).toFixed(2) + "</td>"
+            + "<td>" + (row.area * row.length).toFixed(2) + "</td>"
+            + "<td>" + (row.insulVolume * row.length).toFixed(2) + "</td>"
+            + "<td>" + (row.kapArea * row.length).toFixed(2) + "</td>";
         table.appendChild(tr);
 
-        weightSum += new Number(row.weight * row.len);
-        areaSum += new Number(row.area * row.len);
-        insulSum += new Number(row.insulVolume * row.len);
-        kapSum += new Number(row.kapArea * row.len);
+        weightSum += new Number(row.weight * row.length);
+        areaSum += new Number(row.area * row.length);
+        insulSum += new Number(row.insulVolume * row.length);
+        kapSum += new Number(row.kapArea * row.length);
     }
 
     let tr = document.createElement("tr");
     tr.innerHTML =
-        "<td>合计</td>"
-        + "<td> </td>"
+        "  <td>合计</td>"
+        + "<td>&nbsp</td>"
         + "<td>" + weightSum.toFixed(2) + "</td>"
         + "<td>" + areaSum.toFixed(2) + "</td>"
         + "<td>" + insulSum.toFixed(2) + "</td>"
@@ -106,10 +104,21 @@ window.Router.route("page1", function () {
 
     document.getElementById("page-result").classList.add("show");
 
-    var txt = "1234\n中文12\t456\nAbvdf\t456";
-    var blob = new Blob([txt], { type: "text/plain;charset=utf-8" ,endings:'native'});
-    var link = document.getElementById("report");
-    link.href = URL.createObjectURL(blob);
+    let text = "外径\t\t壁厚\t\t保温厚\t管长\t\t单重\t\t总重\t\t刷漆面积\t保温材料体积\t保护层面积\nmm\t\tmm\t\tmm\t\tm\t\tkg/m\t\tkg\t\tm2\t\tm3\t\t\tm2\n";
+    for (let index = 0; index < rows.length; index++) {
+        const row = rows[index];
+        text += (row.do_ * 1000).toString() + "\t\t";
+        text += (row.thk * 1000).toString() + "\t\t";
+        text += (row.insulThk * 1000).toString() + "\t\t";
+        text += row.length.toString() + "\t\t";
+        text += row.weight .toFixed(2) + "\t\t";
+        text += (row.weight * row.length).toFixed(2) + "\t\t";
+        text += (row.area * row.length).toFixed(2) + "\t\t";
+        text += (row.insulVolume * row.length).toFixed(2) + "\t\t\t";
+        text += (row.kapArea * row.length).toFixed(2) + "\n";
+    }
+
+    toReport(text);
 });
 
 window.Router.route("page2", function () {
@@ -143,7 +152,6 @@ function onPage1AddRow() {
         nextRowId = parseInt(rowStr.slice(index + 1)) + 1;
     } 
 
-    console.log(nextRowId);
     let row = document.createElement("tr");
     row.id = "page1-row-" + nextRowId.toString();
     row.classList.add("page1-row");
@@ -161,4 +169,10 @@ function onPage1AddRow() {
 
 function  onPage1DelRow(id) {
     document.getElementById("page1-row-" + id.toString()).remove();
+}
+
+function toReport(text) {
+    var blob = new Blob([text], { type: "text/plain;charset=utf-8" ,endings:'native'});
+    var link = document.getElementById("report");
+    link.href = URL.createObjectURL(blob);
 }
