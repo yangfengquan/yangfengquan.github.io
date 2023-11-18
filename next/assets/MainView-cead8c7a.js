0bf1596faa8ba8222c8420014f0d5b44b3d020d6
yfq <yangfengquan@126.com>
2023-11-18 13:56:59 +0800

    m

--- next/assets/MainView-cead8c7a.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as r,c as t,t as a}from"./index-badae329.js";const n={};function c(e,s){return r(),t("p",null,a(e.$route.params.module),1)}const _=o(n,[["render",c]]);export{_ as default};
