3805be78bcd1ad62ba40729645fbc8160638348e
yfq000 <yfq000@126.com>
2023-12-20 17:17:42 +0800

    x

--- JS/equipment.js
+++ JS/equipment.js
@@ -1,15 +1,12 @@
-function pumpPower(rows) {
-    let flowRate = parseFloat(rows[2].value);
-    let h = parseFloat(rows[3].value);
-    let rho = parseFloat(rows[4].value);
-    let eta = parseFloat(rows[5].value) / 100;
-
-    if (isNaN(flowRate) || isNaN(h) || isNaN(rho) || isNaN(eta)) {
-        alert("请检查输入！\n输入完成后，需将鼠标移出输入框，并点击表格任意位置！");
-        return false;
-    }
-
-    let p = flowRate * h * 9.81 * rho / 3600 / eta / 1000;
-
-    return [p.toFixed(2)];
+/**
+ * 
+ * @param {*} flowRate 
+ * @param {*} h 
+ * @param {*} rho 
+ * @param {*} eta 
+ * @returns 
+ */
+function pump_power(flowRate, h, rho, eta) {
+    eta /= 100;
+    return flowRate * h * 9.81 * rho / 3600 / eta / 1000;
 }
--- JS/fm.js
+++ JS/fm.js
@@ -181,4 +181,7 @@ export default class Fm {
             }
         }
     }
+    pumpPower() {
+
+    }
 }
