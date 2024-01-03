7b24db3d02dcbb784d5c51d8f76611b757872a81
yfq000 <yfq000@126.com>
2024-01-03 18:54:52 +0800

    m

--- js/insul.js
+++ js/insul.js
@@ -424,63 +424,258 @@ function d1_ball(d0, t0, ts, ta, lambda, alphas) {
     return (0.5 + Math.sqrt(0.25 + 2 / d0 * (lambda / alphas * (t0 - ts) / (ts - ta)))) / (1 / d0);
 }
 
+/**
+ * 最大允许热损失量，GB50264-2013 附录B
+ * @param {number} t0 设备的外表面温度 K
+ * @param {string} tt 全年运行tt="yearly"，季节运行tt="quarter"
+ * @returns {number} 最大允许热损失 W/m2
+ */
 function Qmax_hot(t0, tt) {
+    t0 -= 273.15;
     switch (tt) {
-        case yearly:
-            if (t0 < 50) {
+        case "yearly":
+            if (t0 <= 50) {
                return 52; 
             }
-            else if (t0 > 50 && t0 < 100) {
+            else if (t0 > 50 && t0 <= 100) {
                 return 84;
             }
-            else if (t0 > 100 && t0 < 150) {
+            else if (t0 > 100 && t0 <= 150) {
                 return 104;
             }
-            else if (t0 > 150 && t0 < 200) {
+            else if (t0 > 150 && t0 <= 200) {
                 return 106;
             }
-            else if (t0 > 200 && t0 < 250) {
+            else if (t0 > 200 && t0 <= 250) {
                 return 147;
             }
-            else if (t0 > 250 && t0 < 300) {
+            else if (t0 > 250 && t0 <= 300) {
                 return 167;
             }
-            else if (t0 > 300 && t0 < 350) {
+            else if (t0 > 300 && t0 <= 350) {
                 return 188;
             }
-            else if (t0 > 350 && t0 < 400) {
+            else if (t0 > 350 && t0 <= 400) {
                 return 204;
             }
-            else if (t0 > 400 && t0 < 450) {
+            else if (t0 > 400 && t0 <= 450) {
                 return 220;
             }
-            else if (t0 > 450 && t0 < 500) {
+            else if (t0 > 450 && t0 <= 500) {
                 return 236;
             }
-            else if (t0 > 500 && t0 < 550) {
+            else if (t0 > 500 && t0 <= 550) {
                 return 251;
             }
-            else if (t0 > 550 && t0 < 600) {
+            else if (t0 > 550 && t0 <= 600) {
                 return 266;
             }
-            else if (t0 > 600 && t0 < 650) {
+            else if (t0 > 600 && t0 <= 650) {
                 return 283;
             }
-            else if (t0 > 650 && t0 < 700) {
+            else if (t0 > 650 && t0 <= 700) {
                 return 297;
             }
-            else if (t0 > 700 && t0 < 750) {
+            else if (t0 > 700 && t0 <= 750) {
                 return 311;
             }
-            else if (t0 > 750 && t0 < 800) {
+            else if (t0 > 750 && t0 <= 800) {
                 return 324;
             }
-            else if (t0 > 800 && t0 < 850) {
+            else if (t0 > 800 && t0 <= 850) {
                 return 338;
             }
             break;
-    
+        case "quarter":
+            if (t0 < 50) {
+                return 104; 
+             }
+             else if (t0 > 50 && t0 <= 100) {
+                 return 147;
+             }
+             else if (t0 > 100 && t0 <= 150) {
+                 return 183;
+             }
+             else if (t0 > 150 && t0 <= 200) {
+                 return 220;
+             }
+             else if (t0 > 200 && t0 <= 250) {
+                 return 251;
+             }
+             else if (t0 > 250 && t0 <= 300) {
+                 return 272;
+             }
+             break;
         default:
             break;
     }
+}
+
+/**
+ * 最大允许冷损失量，GB50264-2013 式5.4.2
+ * @param {number} 环境温度 K
+ * @param {number} td 最热月露点温度 K
+ * @param {number} alphas 绝热层外表面与周围空气的换热系数 W/(m2.K)
+ * @returns {number} 最大允许冷损失 W/m2
+ */
+function Qmax_cool(ta, td, alphas) {
+    if (ta - ta <= 4.5) {
+        return -(ta - td) * alphas;
+    } else {
+        return -4.5 * alphas;
+    }  
+}
+
+/**
+ * 圆筒型，单层绝热结构，以每平方米绝热层外表面积表示的冷、热损失量，GB50264-2013 式5.4.3-1
+ * @param {number} d0 管道或设备外径 m
+ * @param {number} d1 绝热层外径 m
+ * @param {number} t0 管道或设备的外表面温度 K
+ * @param {number} ta 环境温度 K
+ * @param {number} lambda 绝热材料导热系数 W/(m.K)
+ * @param {number} alphas 绝热层外表面与周围空气的换热系数 W/(m2.K)
+ * @returns {number} 冷、热损失量 W/m2
+ */
+function Q(d0, d1, t0, ta, lambda, alphas) {
+    return (t0 - ta) / (d1 / (2 * lambda) * Math.log(d1 / d0) + 1 / alphas);
+}
+
+/**
+ * 圆筒型，单层绝热结构，以每米管道长度表示的冷、热损失量，GB50264-2013 式5.4.3-2
+ * @param {number} d1 绝热层外径 m
+ * @param {number} Q 以每平方米绝热层外表面积表示的热损失量，W/m2
+ * @returns {number} 冷、热损失量 W/m
+ */
+function q(d1, Q) {
+    return Math.PI * d1 * Q;
+}
+
+/**
+ * 圆筒型，不同材料双层绝热结构，以每平方米绝热层外表面积表示的冷、热损失量，GB50264-2013 式5.4.3-3
+ * @param {number} d0 管道或设备外径 m
+ * @param {number} d1 内层绝热层外径 m
+ * @param {number} d2 外层绝热层外径 m
+ * @param {number} t0 管道或设备的外表面温度 K
+ * @param {number} ta 环境温度 K
+ * @param {number} lambda1 内层绝热材料导热系数 W/(m.K)
+ * @param {number} lambda2 外层绝热材料导热系数 W/(m.K)
+ * @param {number} alphas 绝热层外表面与周围空气的换热系数 W/(m2.K)
+ * @returns {number} 冷、热损失量 W/m2
+ */
+function Q_2(d0, d1, t0, ta, lambda1, lambda2, alphas) {
+    return (t0 - ta) / (d2 / (2 * lambda1) * Math.log(d1 / d0) + d2 / (2 * lambda2) * Math.log(d2 / d1) + 1 / alphas);
+}
+
+/**
+ * 圆筒型，不同材料双层绝热结构，以每米管道长度表示的冷、热损失量，GB50264-2013 式5.4.3-4
+ * @param {number} d2 外绝热层外径 m
+ * @param {number} Q 以每平方米绝热层外表面积表示的热损失量，W/m2
+ * @returns {number} 冷、热损失量 W/m
+ */
+function q_2(d2, Q) {
+    return Math.PI * d2 * Q;
+}
+
+/**
+ * 平面型，单层绝热结构，以每平方米绝热层外表面积表示的冷、热损失量，GB50264-2013 式5.4.3-5
+ * @param {number} delta 绝热层厚度 m
+ * @param {number} t0 设备的外表面温度 K
+ * @param {number} ta 环境温度 K
+ * @param {number} lambda 绝热材料导热系数 W/(m.K)
+ * @param {number} alphas 绝热层外表面与周围空气的换热系数 W/(m2.K)
+ * @returns {number} 冷、热损失量 W/m2
+ */
+function Q_flat(delta, t0, ta, lambda, alphas) {
+    return (t0 - ta) / (delta / lambda + 1 / alphas);
+}
+
+/**
+ * 平面型，不同材料双层绝热结构，以每平方米绝热层外表面积表示的冷、热损失量，GB50264-2013 式5.4.3-5
+ * @param {number} delta1 内层绝热层厚度 m
+ * @param {number} delta2 外层绝热层厚度 m
+ * @param {number} t0 设备的外表面温度 K
+ * @param {number} ta 环境温度 K
+ * @param {number} lambda1 内层绝热材料导热系数 W/(m.K)
+ * @param {number} lambda2 外层绝热材料导热系数 W/(m.K)
+ * @param {number} alphas 绝热层外表面与周围空气的换热系数 W/(m2.K)
+ * @returns {number} 冷、热损失量 W/m2
+ */
+function Q_flat_2(delta1, delta2, t0, ta, lambda1, lambda2, alphas) {
+    return (t0 - ta) / (delta1 / lambda1 + delta2 / lambda2 + 1 / alphas);
+}
+
+/**
+ * 球型，单层绝热结构，以每平方米绝热层外表面积表示的冷损失量，GB50264-2013 式5.4.3-7
+ * @param {number} d1 绝热层外径 m
+ * @param {number} ts 绝热层外表面温度 K
+ * @param {number} ta 环境温度 K
+ * @param {number} alphas 绝热层外表面与周围空气的换热系数 W/(m2.K)
+ * @returns {number} 冷损失量 W/m2
+ */
+function Q_ball(d1, ts, ta, alphas) {
+    return Math.PI * d1 * d1 * alphas * (ts - ta)
+}
+
+/**
+ * 圆筒、平面，其单、双层绝热结构的外表面温度，GB50264-2013 式5.5.1
+ * @param {number} Q 以每平方米绝热层外表面积表示的冷、热损失量 W/m2
+ * @param {number} ta 环境温度 K
+ * @param {number} alphas 绝热层外表面与周围空气的换热系数 W/(m2.K)
+ * @returns {number} 绝热结构的外表面温度 K
+ */
+function ts_Q(Q, ta, alphas) {
+    return Q / alphas + ta;
+}
+
+/**
+ * 圆筒、其单、双层绝热结构的外表面温度，GB50264-2013 式5.5.2
+ * @param {number} q 以每平方米绝热层外表面积表示的冷、热损失量 W/m2
+ * @param {number} d2 外绝热层外径 m
+ * @param {number} ta 环境温度 K
+ * @param {number} alphas 绝热层外表面与周围空气的换热系数 W/(m2.K)
+ * @returns {number} 绝热结构的外表面温度 K
+ */
+function ts_q(q, d2, ta, alphas) {
+    return q / (Math.PI * d2 * alphas) + ta;
+}
+
+/**
+ * 球形，其单层绝热结构的外表面温度，GB50264-2013 式5.5.3
+ * @param {number} Q 以每平方米绝热层外表面积表示的冷、热损失量 W/m2
+ * @param {number} d1 绝热层外径 m
+ * @param {number} ta 环境温度 K
+ * @param {number} alphas 绝热层外表面与周围空气的换热系数 W/(m2.K)
+ * @returns {number} 绝热结构的外表面温度 K
+ */
+function ts_ball_Q(Q, d1, ta, alphas) {
+    return Q / (Math.PI * d1 * d1 * alphas) + ta;
+}
+
+/**
+ * 圆筒型，不同材料双层绝热结构层间界面处温度，GB50264-2013 式5.6.1
+ * @param {number} d0 管道或设备外径 m
+ * @param {number} d1 内层绝热层外径 m
+ * @param {number} d2 外层绝热层外径 m
+ * @param {number} t0 管道或设备的外表面温度 K
+ * @param {number} ts 绝热层外表面温度 K
+ * @param {number} lambda1 内层绝热材料导热系数 W/(m.K)
+ * @param {number} lambda2 外层绝热材料导热系数 W/(m.K)
+ * @returns {number} 绝热结构层间界面处温度 K
+ */
+function t1(d0, d1, d2, t0, ts, lambda1, lambda2) {
+    return (lambda1 * t0 * Math.log(d2 / d1) + lambda2 * ts * Math.log(d1 / d0)) / (lambda1 * Math.log(d2 / d1) + lambda2 * Math.log(d1 / d0));
+}
+
+/**
+ * 平面型，不同材料双层绝热结构层间界面处温度，GB50264-2013 式5.6.2
+ * @param {number} delta1 内层绝热层厚度 m
+ * @param {number} delta2 外层绝热层厚度 m
+ * @param {number} t0 管道或设备的外表面温度 K
+ * @param {number} ts 绝热层外表面温度 K
+ * @param {number} lambda1 内层绝热材料导热系数 W/(m.K)
+ * @param {number} lambda2 外层绝热材料导热系数 W/(m.K)
+ * @returns {number} 绝热结构层间界面处温度 K
+ */
+function t1_flat(delta1, delta2, t0, ts, lambda1, lambda2) {
+    return (lambda1 * t0 * delta2 + lambda2 * ts * lambda1) / (lambda1 * delta2 + lambda2 * lambda1);
 }
